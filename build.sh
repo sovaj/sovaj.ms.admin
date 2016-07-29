0c583804b242c4eda9850f5e1a3978b96b677177
sovaj <Mickael.Dubois@pj.ca>
2016-07-29 15:25:52 -0400

    more clean up

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 _tag=$1
 
 if [ -z "${_tag}" ]; then
-    source _VERSION   
+    source _VERSION
     _tag=${_VERSION}
 fi
 
-docker build --tag "ng2-admin:${_tag}"  --no-cache=true .
+docker build --tag "sovaj.ms.admin:${_tag}"  --no-cache=true .
